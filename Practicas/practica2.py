4713a3b6fcab33d26b6c686b4aa7019e96b38adf
juanboscop <juanbosco.pavia@gmail.com>
2022-01-07 13:22:29 +0100

    Practicas mejoradas

--- Practicas/practica2.py
+++ Practicas/practica2.py
@@ -1,1 +1,21 @@
-#es la misma que la primera
+def num():
+    print("\n\tWelcome to bosco's number list app\n")
+    numlist = []
+    ñ = int(input("Enter how many numbers you want on your list : "))
+    for times in range(ñ): 
+        numlist.append(int(input("Enter number: ")))
+
+    
+    for i in numlist:
+        numlist.count(i)
+        numlist.remove(i)
+            
+
+    print(numlist)
+        
+
+    
+
+num()
+
+   
